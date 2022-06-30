1619223c5e6cb445386db076211b8b3aae7ae547
Christochi <tochukwui2@gmail.com>
2022-06-30 12:02:07 -0400

    scipts for pulling certificates

---
+++ ansible/roles/configure_vault/files/init-vault-tls.sh
@@ -0,0 +1,8 @@
+secret_result=$(~/.local/bin/az keyvault secret show --id "${key_vault_secret_id}" --query "value" --output tsv)
+
+echo $secret_result | base64 -d | openssl pkcs12 -clcerts -nokeys -passin pass: | openssl x509 -out /opt/vault/tls/vault-cert.pem
+
+echo $secret_result | base64 -d | openssl pkcs12 -cacerts -nokeys -chain -passin pass: | openssl x509 -out /opt/vault/tls/vault-ca.pem
+
+echo $secret_result | base64 -d | openssl pkcs12 -nocerts -nodes -passin pass: | openssl pkcs8 -nocrypt -out /opt/vault/tls/vault-key.pem
+
