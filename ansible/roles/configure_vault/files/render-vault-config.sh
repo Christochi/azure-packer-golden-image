d1d3ec34573fd86b830ad9d7cfceaedef8f35522
Christochi <tochukwui2@gmail.com>
2022-06-30 12:33:43 -0400

    scripts for rendering templated vars

---
+++ ansible/roles/configure_vault/files/render-vault-config.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# ╭──────────────────────────────────────────────────────────╮
+# │          ensure root is not runnint this script          │
+# ╰──────────────────────────────────────────────────────────╯
+if [[ $EUID -eq 0 ]]; then
+   echo "You must run this script as non-root user with sudo permission." 1>&2
+   exit 1
+fi
+export INSTANCE_NAME="$(curl -sH Metadata:true --noproxy '*' 'http://169.254.169.254/metadata/instance/compute/name?api-version=2020-09-01&format=text')"
+export LOCAL_IPV4="$(curl -sH Metadata:true --noproxy '*' 'http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2020-09-01&format=text')"
+# ╭──────────────────────────────────────────────────────────╮
+# │         check env vars are present and populated         │
+# ╰──────────────────────────────────────────────────────────╯
+if [ -z "${INSTANCE_NAME+x}" ] || [ -z "${INSTANCE_NAME}" ]; then
+  echo >&2 "*** the 'INSTANCE_NAME' environment variable is required" ;
+  exit 1 ;
+fi
+if [ -z "${SUBSCRIPTION_ID+x}" ] || [ -z "${SUBSCRIPTION_ID}" ]; then
+  echo >&2 "*** the 'SUBSCRIPTION_ID' environment variable is required" ;
+  exit 1 ;
+fi
+if [ -z "${RESOURCE_GROUP_NAME+x}" ] || [ -z "${RESOURCE_GROUP_NAME}" ]; then
+  echo >&2 "*** the 'RESOURCE_GROUP_NAME' environment variable is required" ;
+  exit 1 ;
+fi
+if [ -z "${VM_SCALE_SET_NAME+x}" ] || [ -z "${VM_SCALE_SET_NAME}" ]; then
+  echo >&2 "*** the 'VM_SCALE_SET_NAME' environment variable is required" ;
+  exit 1 ;
+fi
+if [ -z "${LEADER_TLS_SERVERNAME+x}" ] || [ -z "${LEADER_TLS_SERVERNAME}" ]; then
+  echo >&2 "*** the 'LEADER_TLS_SERVERNAME' environment variable is required" ;
+  exit 1 ;
+fi
+# ╭──────────────────────────────────────────────────────────╮
+# │                render and store template                 │
+# ╰──────────────────────────────────────────────────────────╯
+while read "$tmpl";do
+  cat "${tmpl}" | envsubst > "/etc/vault.d/$(basename "${tmpl}").hcl"
+done < <(find "/var/vault.d" -type f -name "*.tmpl")
